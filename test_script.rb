7d8cc0cf8efd7bb2e2283e3e068a07ec48eed67d
rajapads <rajapads@sify.com>
2014-08-13 15:25:23 +0530

    newupdatedone

--- test_script.rb
+++ test_script.rb
@@ -3,6 +3,6 @@ print " tets "
 print " good night"
 print "Do you want to add or delete SSH keys [Add/Delete]: "
 git init
-git clone git://github.com/rajapads/katest
+git clone git://github.com/rajapads/testscript
 
 
